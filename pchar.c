6c130d37745a8a9c03f581c9862c41bcdd928cd0
nat006 <nthatipmoloi006@gmail.com>
2023-11-16 11:21:44 -0800

    pchar.c

--- pchar.c
+++ pchar.c
@@ -1,31 +1,32 @@
 #include "monty.h"
+
 /**
- * f_pchar - prints the char at the top of the stack,
- * followed by a new line
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+ * f_pchar - Print the char at the top of the stack, followed by a new line
+ * @head: Stack head
+ * @counter: Line number
+ * Return: No return
+ */
 void f_pchar(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
+    stack_t *current = *head;
 
-	h = *head;
-	if (!h)
-	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	if (h->n > 127 || h->n < 0)
-	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	printf("%c\n", h->n);
+    if (!current)
+    {
+        fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    if (current->n > 127 || current->n < 0)
+    {
+        fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%c\n", current->n);
 }
