4395647a39bdbb0cc9537330604c78dcd266708f
nat006 <nthatipmoloi006@gmail.com>
2023-11-16 11:16:25 -0800

    mod.c

--- mod.c
+++ mod.c
@@ -1,41 +1,56 @@
 #include "monty.h"
-/**
- * f_mod - computes the rest of the division of the second
- * top element of the stack by the top element of the stack
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_mod(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	aux = h->next->n % h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+/**
+ * get_stack_length - Get the length of the stack
+ * @head: Stack head
+ * Return: Length of the stack
+ */
+int get_stack_length(stack_t **head)
+{
+    stack_t *current = *head;
+    int length = 0;
+
+    while (current)
+    {
+        current = current->next;
+        length++;
+    }
+
+    return length;
+}
+
+/**
+ * mod - Modulus function
+ * @head: Stack head
+ * @counter: Line number
+ * Return: No return
+ */
+void mod(stack_t **head, unsigned int counter)
+{
+    stack_t *current = *head;
+    int length = get_stack_length(head);
+    int remainder;
+
+    if (length < 2)
+    {
+        fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    if (current->n == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    remainder = current->next->n % current->n;
+    current->next->n = remainder;
+    *head = current->next;
+    free(current);
 }
