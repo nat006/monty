73e8d4d75aa70f816a9e7bda0ef46d1cd44eb865
nat006 <nthatipmoloi006@gmail.com>
2023-11-16 11:29:05 -0800

    pstr.c

--- pstr.c
+++ pstr.c
@@ -1,25 +1,25 @@
 #include "monty.h"
-/**
- * f_pstr - prints the string starting at the top of the stack,
- * followed by a new
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_pstr(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
 
-	h = *head;
-	while (h)
-	{
-		if (h->n > 127 || h->n <= 0)
-		{
-			break;
-		}
-		printf("%c", h->n);
-		h = h->next;
-	}
-	printf("\n");
+/**
+ * pstr - Print the string starting at the top of the stack, followed by a new line
+ * @head: Stack head
+ * @counter: Line number
+ * Return: No return
+ */
+void pstr(stack_t **head, unsigned int counter)
+{
+    stack_t *current = *head;
+    (void)counter;
+
+    while (current)
+    {
+        if (current->n > 127 || current->n <= 0)
+        {
+            break;
+        }
+        printf("%c", current->n);
+        current = current->next;
+    }
+
+    printf("\n");
 }
