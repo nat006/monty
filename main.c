06104e1f753e34596eeb7b0659d4d1eb475d056e
nat006 <nthatipmoloi006@gmail.com>
2023-11-16 09:25:31 -0800

    main.c

--- main.c
+++ main.c
@@ -1,13 +1,32 @@
-#include "monty.h"
+#include "monty"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main(void)
+
+char **op_toks = NULL;
+
+
+/**
+ * main - the entry point for Monty Interp
+ *
+ * @argc: the count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ */
+int main(int argc, char **argv)
 {
-    /* Your code */
+    FILE *script_fd = NULL;
+    int exit_code = EXIT_SUCCESS;
 
-    return 0;
-}
 
-void free_stack(stack_t *stack)
-{
-    /* Implementation of free_stack */
+    if (argc != 2)
+        return (usage_error());
+    script_fd = fopen(argv[1], "r");
+    if (script_fd == NULL)
+        return (f_open_error(argv[1]));
+    exit_code = run_monty(script_fd);
+    fclose(script_fd);
+    return (exit_code);
 }
