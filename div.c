ad12d296a5ee9107b5bf2bc2369ad1cfddadc580
nat006 <nthatipmoloi006@gmail.com>
2023-11-16 11:00:11 -0800

    div.c

--- div.c
+++ div.c
@@ -1,40 +1,56 @@
 #include "monty.h"
+
 /**
- * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+ * get_stack_length - Get the length of the stack
+ * @head: Stack head
+ * Return: Length of the stack
+ */
+int get_stack_length(stack_t **head)
+{
+    stack_t *current = *head;
+    int length = 0;
+
+    while (current)
+    {
+        current = current->next;
+        length++;
+    }
+
+    return length;
+}
+
+/**
+ * f_div - Division function
+ * @head: Stack head
+ * @counter: Line number
+ * Return: No return
+ */
 void f_div(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	int len = 0, aux;
+    stack_t *current = *head;
+    int length = get_stack_length(head);
+    int division;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+    if (length < 2)
+    {
+        fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    if (current->n == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    division = current->next->n / current->n;
+    current->next->n = division;
+    *head = current->next;
+    free(current);
 }
