97d16efc33b6ee0d4b64b197702caff5229747c0
nat006 <nthatipmoloi006@gmail.com>
2023-11-16 11:06:50 -0800

    mul.c

--- mul.c
+++ mul.c
@@ -1,32 +1,48 @@
 #include "monty.h"
+
+
 /**
- * f_mul - multiplies the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+ * get_stack_length - Get the length of the stack
+ * @head: Stack head
+ * Return: Length of the stack
+ */
+int get_stack_length(stack_t **head)
+{
+    stack_t *current = *head;
+    int length = 0;
+
+    while (current)
+    {
+        current = current->next;
+        length++;
+    }
+
+    return length;
+}
+
+/**
+ * f_mul - Multiplication function
+ * @head: Stack head
+ * @counter: Line number
+ * Return: No return
+ */
 void f_mul(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	int len = 0, aux;
+    stack_t *current = *head;
+    int length = get_stack_length(head);
+    int product;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	aux = h->next->n * h->n;
-	h->next->n = aux;
-	*head = h->next;
-	free(h);
+    if (length < 2)
+    {
+        fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    product = current->next->n * current->n;
+    current->next->n = product;
+    *head = current->next;
+    free(current);
 }
