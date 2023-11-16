6205f13f11b39a25d0435e3c871154893d64741a
nat006 <nthatipmoloi006@gmail.com>
2023-11-16 11:42:13 -0800

    main.c

--- main.c
+++ main.c
@@ -1,45 +1,26 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
-/**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main(int argc, char *argv[])
+int main(void)
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-return (0);
+    stack_t *head = NULL;
+    unsigned int counter = 1;
+    
+    /* Example usage */
+    printf("Creating initial stack...\n");
+    push(&head, 1);
+    push(&head, 2);
+    push(&head, 3);
+    
+    printf("Printing stack before rotation:\n");
+    print_stack(head);
+    
+    printf("Rotating stack to the top...\n");
+    f_rotl(&head, counter);
+    
+    printf("Printing stack after rotation:\n");
+    print_stack(head);
+    
+    printf("Freeing stack...\n");
+    free_stack(head);
+    
+    return 0;
 }
