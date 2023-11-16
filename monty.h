71f45865778746a9f1953bef18724921d65e4a78
nat006 <nthatipmoloi006@gmail.com>
2023-11-16 11:57:56 -0800

    monty.h

--- monty.h
+++ monty.h
@@ -54,22 +54,12 @@ typedef struct instruction_s
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
-void push(stack_t **head, unsigned int number);
+
 void pall(stack_t **head, unsigned int number);
 void pint(stack_t **head, unsigned int number);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
-void pop(stack_t **head, unsigned int counter);
-void swap(stack_t **head, unsigned int counter);
-void add(stack_t **head, unsigned int counter);
-void nop(stack_t **head, unsigned int counter);
-void sub(stack_t **head, unsigned int counter);
-void div(stack_t **head, unsigned int counter);
-void mul(stack_t **head, unsigned int counter);
-void mod(stack_t **head, unsigned int counter);
-void pchar(stack_t **head, unsigned int counter);
-void pstr(stack_t **head, unsigned int counter);
-void rotl(stack_t **head, unsigned int counter);
+
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
@@ -78,6 +68,18 @@ void f_stack(stack_t **head, unsigned int counter);
 
 void push(stack_t **head, int n);
 void print_stack(stack_t *head);
-void free_stack(stack_t *head);
+
+void add(stack_t **head, unsigned int counter);
+void sub(stack_t **head, unsigned int counter);
+void mul(stack_t **head, unsigned int counter);
+void div(stack_t **head, unsigned int counter);
+void mod(stack_t **head, unsigned int counter);
+void pchar(stack_t **head, unsigned int counter);
+void pstr(stack_t **head, unsigned int counter);
 void rotl(stack_t **head, unsigned int counter);
+void rotr(stack_t **head, unsigned int counter);
+void swap(stack_t **head, unsigned int counter);
+void nop(stack_t **head, unsigned int counter);
+void pop(stack_t **head, unsigned int counter);
+
 #endif
