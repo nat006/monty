b300b07e4132a1fbcc620e49c7f2c7e3e8d64222
nat006 <nthatipmoloi006@gmail.com>
2023-11-16 10:37:51 -0800

    swap.c

--- swap.c
+++ swap.c
@@ -1,31 +1,46 @@
 #include "monty.h"
-/**
- * f_swap - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_swap(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int len = 0, aux;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	aux = h->n;
-	h->n = h->next->n;
-	h->next->n = aux;
+/**
+ * get_stack_length - Get the length of the stack
+ * @head: Stack head
+ * Return: Length of the stack
+ */
+int get_stack_length(stack_t **head)
+{
+    stack_t *h = *head;
+    int len = 0;
+
+    while (h)
+    {
+        h = h->next;
+        len++;
+    }
+
+    return len;
+}
+
+/**
+ * swap - Swaps the top two elements of the stack
+ * @head: Stack head
+ * @counter: Line number
+ * Return: No return
+ */
+void swap(stack_t **head, unsigned int counter)
+{
+    stack_t *h = *head;
+    int len = get_stack_length(head);
+    int aux;
+
+    if (len < 2)
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    aux = h->n;
+    h->n = h->next->n;
+    h->next->n = aux;
 }
